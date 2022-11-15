3dabd240a041cc2e1751ee1f3091f21682f464dd
phillip-che <pche@cpp.edu>
2022-11-14 20:05:47 -0800

    first commit

---
+++ project2.java
@@ -0,0 +1,18 @@
+public class project2 {
+
+    public static void main(String[] args) {
+
+    }
+    
+    public static void dijkstra() {
+
+    }
+
+    public static void floydWarshall() {
+
+    }
+
+    public static void generateGraph(int n) {
+        
+    }
+}
