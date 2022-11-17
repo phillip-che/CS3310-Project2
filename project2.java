cdbce4677ccb7b6d36141149a4923bd2276bbe6d
phillip-che <pche@cpp.edu>
2022-11-16 20:53:06 -0800

    Implemented Graph Generator Methods

--- project2.java
+++ project2.java
@@ -1,18 +1,41 @@
+import java.util.*;
+
 public class project2 {
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.deepToString(denseGraphGen(3)));
+        System.out.println(Arrays.deepToString(sparseGraphGen(3)));
     }
     
-    public static void dijkstra() {
+    public static void dijkstra(int[][] graph, int[] weights, int start) {
 
     }
 
-    public static void floydWarshall() {
-
-    }
-
-    public static void generateGraph(int n) {
+    public static void floydWarshall(int[][] graph, int[] weights) {
         
+    }
+
+    public static int[][] denseGraphGen(int n) {
+        int[][] res = new int[n][n];
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i != j) {
+                    res[i][j] = (int) (Math.random() * 20)+1;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static int[][] sparseGraphGen(int n) {
+        int[][] res = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            res[i][(i+1)%n] = (int) (Math.random() * 20)+1;
+        }
+        
+        return res;
     }
 }
